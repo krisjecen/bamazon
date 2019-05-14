4354f06238bacb299f729edb4daef34797ef81a4
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-05-13 22:19:09 -0700

    added initial js file with comments outlining the assignment, and package.json

---
+++ bamazonCustomer.js
@@ -0,0 +1,20 @@
+// requirements
+var mysql = require('mysql');
+var inquirer = require('inquirer');
+
+// need to connect to my bamazon database
+
+// display all items for sale to the customer when the app is run
+
+// prompt customer: ask for sku of the product they want to buy
+
+// prompt customer: ask for quantity of the product they want to buy
+
+// check to see if sufficient stock exists for the order
+    // if insufficient, display "insufficient quantity" or similar
+
+
+// fulfill the customer's order by decreasing the stock (update db)
+
+// calculate and display the total cost of their purchase
+
