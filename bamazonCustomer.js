c9c898938625fa613f6459c0fffdce104b37b493
Kris Jecen <13891670+krisjecen@users.noreply.github.com>
2019-05-17 19:18:03 -0700

    added display of full inventory with cli-table upon running program

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,8 @@
 // requirements
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var Table = require('cli-table');
+
 
 // need to connect to my bamazon database
 var connection = mysql.createConnection({
@@ -17,11 +19,55 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err
     console.log('connected as id ' + connection.threadId)
-    connection.end()
   });
 
 
 // display all items for sale to the customer when the app is run
+
+// -----------------------------------------------------------------------------
+// function to display all items for sale upon starting app
+// -----------------------------------------------------------------------------
+function displayFullInventory () {
+    var inventoryHeaders = [];
+
+    connection.query('DESCRIBE products', function (err, res) {
+        if (err) throw err;
+
+        for (var headerCount = 0; headerCount < res.length; headerCount++) {
+            let columnHeader = res[headerCount].Field;
+            inventoryHeaders.push(columnHeader)
+        };
+        
+    });
+
+    connection.query('SELECT * FROM products', function (err, res) {
+      if (err) throw err
+
+      // instantiate
+      var fullInventory = new Table({
+        head: inventoryHeaders,
+        colWidths: [5, 40, 40, 10, 10]
+      });
+    
+      // write a for loop that walks through the data object we get from the SELECT
+      // table is an Array, so you can `push`, `unshift`, `splice` and friends
+      for (var i = 0; i < res.length; i++) {
+        // makes an array of each item that we read through in the response
+        let inventoryItem = [res[i].sku, res[i].product_name, res[i].department_name, res[i].price, res[i].qty];
+        // adds each inventory item to our array
+        fullInventory.push(inventoryItem)
+      }
+      
+      // displays the full inventory to the customer
+      console.log(fullInventory.toString());
+
+      connection.end()
+    });
+};
+
+
+// upon running the program with node bamazonCustomer
+displayFullInventory();
 
 // prompt customer: ask for sku of the product they want to buy
 
